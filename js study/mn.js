b535536a0ef3c936b4b5692e82bc6b5debdfed2b
Gladwin <75156799+xakep-8@users.noreply.github.com>
2024-08-28 11:31:52 +0530

    added prettier and started formating

--- js study/mn.js
+++ js study/mn.js
@@ -1,21 +1,15 @@
-const student ={
-    name: 'John Doe',
-    age: 20,
-    grade: 'A',
-    is_passed: true
-};
-let name1 = "Alice";
-let age = 30;
+const vowel = (str) => {
+    let count = 0
+    const  vowela =['a','e','i','o','u']
+    for ( let char of str){
+        if (vowela.includes(char))
+        {
+            count++
 
-// Output the variable using alert with template literals
-alert(`Hello, my name is ${name1} and I am ${age} years old.`);
-let gamen=23;
-let userno =prompt("enter the number");
-while(userno != gamen){
-          userno =prompt("enter the number to match");
-    }
-        for (let i=1; i<userno; i++){
-            alert( `the number is ${i}`);
         }
-    alert("congratss.");
-  console.log(`Congratulation the entererd no is matched : ${userno}` +userno+"!!!!!!!!!!!!");
+    
+    }
+    console.log(count)
+    return count
+}
+console.log (vowel("sfsdfsrfaio"))
